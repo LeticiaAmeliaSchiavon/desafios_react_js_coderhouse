c1b4c2e7dcc18015438997829d41a9e5db4c7a53
Leticia Schiavon <leticiaschiavon20@gmail.com>
2024-09-19 10:29:45 -0400

    contador com botão

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemCount from './ItemCount';
 
 const ItemListContainer = ({ greeting }) => {
+    const handleAdd = (quantity) => {
+        console.log(`Adicionado ${quantity} itens ao carrinho`);
+    };
+
     return (
         <div className="container mt-4">
             <h2>{greeting}</h2>
+            <ItemCount stock={10} initial={1} onAdd={handleAdd} />
         </div>
     );
-}
+};
 
 export default ItemListContainer;
